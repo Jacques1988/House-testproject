42bca7eb69b8964904a2801241f79b4efca2307e
Jacques <jacquesvanluyck@gmail.com>
2023-04-02 14:03:23 +0200

    update windows and sliding doors

--- src/app/house/Objects/houses/BungalowPurea/middleBlock/Kitchen.class.ts
+++ src/app/house/Objects/houses/BungalowPurea/middleBlock/Kitchen.class.ts
@@ -5,11 +5,21 @@ import Window from '../window/Window.class';
 export default class Kitchen{
     kitchenBlock: any = new Room(5, 3, 5, 100, 100).buildRoom();
     kitchen:any = new THREE.Group();
-    window1: any = new Window(3, 1, 100, 100).buildWindow(0, 0.5, 2.5);
+    window1: any = new Window(2, 1, 100, 100).buildWindow(0.5, 0.5, 2.5);
+    slidingDoor:any = new Window(3, 3.5).buildSlidingDoor(2.5, -0.3, 0, 500, 500)
 
     constructor(){
-        this.kitchen.add(this.kitchenBlock, this.window1);
+        this.kitchen.add(
+            this.kitchenBlock, 
+            this.window1,
+            this.slidingDoor
+            );
+
+        this.slidingDoor.rotation.y = Math.PI * 0.5;
         this.kitchen.position.set(6.5, 2, 0.5);
+
+        this.kitchen.minfilter = THREE.NearestFilter;
+        this.window1.minFilter = THREE.NearestFilter;
     }
 
     buildKitchen(){
--- src/app/house/Objects/houses/BungalowPurea/middleBlock/LivingRoom.class.ts
+++ src/app/house/Objects/houses/BungalowPurea/middleBlock/LivingRoom.class.ts
@@ -1,14 +1,27 @@
 import * as THREE from 'three';
 import Room from '../../Room.Class';
+import Window from '../window/Window.class';
 
 
 export default class LivingRoom {
     livingRoom: any = new Room(4, 3, 7, 100, 100).buildRoom();
     livingArea: any = new THREE.Group();
+    slidingDoorBackside1:any = new Window(3, 3.4).buildSlidingDoor(0, -0.3, -3.5, 500, 500);
+    slidingDoorSide1:any = new Window(3, 3.4).buildSlidingDoor(2, -0.3, -1.5, 500, 500);
+    slidingDoorSide2:any = new Window(3, 3.4).buildSlidingDoor(2, -0.3, 1, 500, 500);
 
     constructor() {
+        this.slidingDoorBackside1.rotation.y = Math.PI;
+        this.slidingDoorSide1.rotation.y = Math.PI * 0.5;
+        this.slidingDoorSide2.rotation.y = Math.PI * 0.5;
 
-        this.livingArea.add(this.livingRoom);
+        this.livingArea.add(
+            this.livingRoom,
+            this.slidingDoorBackside1,
+            this.slidingDoorSide1,
+            this.slidingDoorSide2,
+            );
+
         this.livingArea.position.set(6, 2, -5.5);
     }
 
